53ce4364f7afd921174e2522b4b536c47e385f0d
Kyle Welton <->
2024-02-26 14:52:57 -0600

    update test

--- src/pages/confirmed-booking-page/confirmed-booking-page.test.js
+++ src/pages/confirmed-booking-page/confirmed-booking-page.test.js
@@ -3,6 +3,6 @@ import ConfirmedBookingPage from "./confirmed-booking-page.component";
 
 test("renders the ConfirmedBookingPage heading", () => {
   render(<ConfirmedBookingPage />);
-  const headingElement = screen.getByText("Booking confirmed!");
+  const headingElement = screen.getByText("Reservation confirmed!");
   expect(headingElement).toBeInTheDocument();
 });
